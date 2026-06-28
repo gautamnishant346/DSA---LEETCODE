148d271c9fb0e5d540353b7cf492d810e0b1ddd2
NISHANT <148745958+gautamnishant346@users.noreply.github.com>
2026-06-28 10:46:55 +0530

    Time: 2 ms (9.57%) | Memory: 8.6 MB (35.58%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int EvenSum = 0;
+        int OddSum = 0;
+        for(int i = 0; i<num.size(); i++){
+            if(i%2 == 0)
+             EvenSum+= num[i] - '0';   // num[i] character hai digit nhi (ASCII value add ho rhi hai)
+            else
+             OddSum+= num[i] - '0';   // 0 subtract krna se character digit integer bn jati hai
+        }
+        if(EvenSum == OddSum){
+            return true;
+        }
+        return false;
+    }
+};
