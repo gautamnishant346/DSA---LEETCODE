e6f7902f83fc48d296f2f04a7341c96c4f678d2e
NISHANT <148745958+gautamnishant346@users.noreply.github.com>
2026-06-30 18:52:31 +0530

    Time: 2 ms (50.89%) | Memory: 10 MB (11.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,15 +3,15 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size())   // Agar lengths same nhi ha to Anagram kbhi nhi ho skta
          return false;
-        unordered_map<char,int> mp1;
-        unordered_map<char,int> mp2;
-        for(int i=0; i<s.size(); i++)
-         mp1[s[i]]++;
-        for(int i=0; i<t.size(); i++)
-         mp2[t[i]]++;
-        
-        if(mp1 == mp2)
-         return true;
-        return false;
+        unordered_map<char,int> mp;
+        // s.size() == t.size() ya fir n = s.size();
+        for(int i=0; i<s.size(); i++){   // loop s ka liye nhi index k liye lga hai
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto it : mp)
+        if(it.second != 0)
+         return false;
+        return true;
     }
 };
