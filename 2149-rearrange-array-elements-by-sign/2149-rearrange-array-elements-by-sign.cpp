c5d6a52972146eb838b1c3b8aec6b5a1676b7bb9
NISHANT <148745958+gautamnishant346@users.noreply.github.com>
2026-08-09 23:40:30 +0530

    Time: 4 ms (77.27%), Space: 128.2 MB (95.64%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -6,7 +6,7 @@ public:
         int pos = 0;
         int neg = 1;
         for(int i=0; i<n; i++){
-            if(nums[i] > 0){
+            if(nums[i] > 0){ 
                 result[pos] = nums[i];
                 pos += 2;
             }
