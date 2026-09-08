383c55e5d9851a40bf61458069a89b9067f2e0f2
NISHANT <148745958+gautamnishant346@users.noreply.github.com>
2026-09-08 17:09:06 +0530

    Time: 2 ms (36.36%), Space: 12.7 MB (20.5%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -5,15 +5,15 @@ void fun(vector<int>& nums,int i,vector<int>& temp,vector<vector<int>>& ans){
         ans.push_back(temp);
         return;
     }
-    // Include
-    temp.push_back(nums[i]);
+    // nhi lege to direct aaga jump krege
     fun(nums,i+1,temp,ans);
 
-    // Exclude
-    //Wapas aakar hatao
-    temp.pop_back();
-    // mat lo
+    // lege -> pehle temp ma add fir aaga bdege
+    temp.push_back(nums[i]);
     fun(nums,i+1,temp,ans);
+    // Wapas aana ka liye
+    temp.pop_back();
+
 }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
